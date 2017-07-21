6666707b3561df7f279e895c2c4b355a85fc6beb
Salinder Sidhu <salinder.sid@gmail.com>
2017-07-21 18:22:02 -0400

    Implemented Addon utility interface

---
+++ cmd/addon/utils.go
@@ -0,0 +1,7 @@
+package addon
+
+/*
+Addon represents an interface containing web and file utilities for downloading
+and managing World of Warcraft addons
+*/
+type Addon interface{}
--- cmd/commands.go
+++ cmd/commands.go
@@ -2,4 +2,5 @@ package cmd
 
 import "github.com/urfave/cli"
 
+// Commands represents a collection of CLI command objects
 var Commands = []cli.Command{}
